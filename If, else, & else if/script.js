eb3780ff002f94347d12f62001643c681b09fcdb
Abdul Moiz <138908943+codewithmoiz@users.noreply.github.com>
2023-10-21 18:39:58 +0500

    Add files via upload

---
+++ If, else, & else if/script.js
@@ -0,0 +1,75 @@
+// Task 2
+// var num1 = prompt("Enter the first number: ");
+// var num2 = prompt("Enter the second number: ");
+
+// if (num1 === num2) {
+//   alert("The two numbers are equal.");
+// } else {
+//   if (num1 > num2) {
+//     alert("The larger number is " + num1 + ".");
+//   } else {
+//     alert("The larger number is " + num2 + ".");
+//   }
+// }
+
+// Task 3
+// var number = prompt("Enter a number: ");
+
+// if (number > 0) {
+//   alert("The number is positive.");
+// } else if (number < 0) {
+//   alert("The number is negative.");
+// } else {
+//   alert("The number is zero.");
+// }
+
+// Task 4
+// var vowel = prompt("Enter a letter");
+
+// if(vowel == "a" || vowel == "e" || vowel == "i" || vowel == "o" || vowel == "u"){
+//     alert("Vowel")
+// }
+// else{
+//     alert("Constant")
+// }
+
+// Task 5
+// var correctPassword = "mypassword";
+// var userPassword = prompt("Enter your password: ");
+
+// if (userPassword === "") {
+//   alert("Please enter your password.");
+// } else if (userPassword === correctPassword) {
+//   alert("Correct! The password you entered matches the original password.");
+// } else {
+//   alert("Incorrect password.");
+// }
+
+// Task 6
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// Task 7
+// var enterTime = prompt("Enter the time in 24 hours clock format (HH:MM): ","Like this 1900");
+
+// if(enterTime >= 0000 && enterTime < 1200){
+//     alert("Good Morning!")
+// }
+// else if(enterTime >= 1200 && enterTime < 1700){
+//     alert("Good Afternoon!")
+// }
+// else if(enterTime >= 1700 && enterTime < 2100){
+//     alert("Good Evening!")
+// }
+// else if(enterTime >= 2100 && enterTime < 2359){
+//     alert("Good Night!")
+// }
+// else{
+//     alert("Something is wrong")
+// }
